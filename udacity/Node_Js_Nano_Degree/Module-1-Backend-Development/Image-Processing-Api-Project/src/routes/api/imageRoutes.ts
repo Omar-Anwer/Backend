34f55e377b630b9dc6527e45d2d61211b0ee6df2
Omar Anwer <omar.anwer@thirdwayv.com>
2024-11-19 00:28:08 +0200

    Apply image processing feature

--- udacity/Node_Js_Nano_Degree/Module-1-Backend-Development/Image-Processing-Api-Project/src/routes/api/imageRoutes.ts
+++ udacity/Node_Js_Nano_Degree/Module-1-Backend-Development/Image-Processing-Api-Project/src/routes/api/imageRoutes.ts
@@ -1,10 +1,70 @@
 import express, { Request, Response } from 'express';
+import path from 'node:path';
+import fs from 'node:fs';
+import sharp from 'sharp';
 
 const imageRoute = express.Router();
 
-imageRoute.get('/', (req: Request, res: Response): void => {
-    console.log(req.query);
-    res.status(200).send('image route');
+const imagesDir = path.join(__dirname, '../../../public/images/');
+
+imageRoute.get('/', async (req: Request, res: Response) => {
+    const { filename, width, height } = req.query;
+
+    // Validate query parameters
+    if (!filename || !width || !height) {
+        res.status(400).json({
+            error: 'Missing required query parameters: filename, width, and height',
+        });
+        return;
+    }
+
+    // Validate filename
+    const imagePath = path.join(imagesDir, 'full/', `${filename}.jpg`);
+
+    if (!fs.existsSync(imagePath)) {
+        res.status(404).json({
+            error: `Image '${filename}' not found in ${imagePath}`,
+        });
+        return;
+    }
+
+    // Validate dimensions
+    const widthInt = parseInt(width as string, 10);
+    const heightInt = parseInt(height as string, 10);
+
+    if (isNaN(widthInt) || isNaN(heightInt)) {
+        res.status(400).json({ error: 'Invalid width, and height' });
+        return;
+    }
+
+    if (widthInt <= 0 || heightInt <= 0) {
+        res.status(400).json({
+            error: 'Width and height must be positive integers',
+        });
+        return;
+    }
+    const resizedImagePath = path.join(
+        imagesDir,
+        'thumb/',
+        `${filename}-${widthInt}x${heightInt}.jpg`,
+    );
+    console.log('resizedImagePath:', resizedImagePath);
+
+    try {
+        // Resize the image
+        await sharp(imagePath)
+            .resize(widthInt, heightInt)
+            .toFile(resizedImagePath);
+        // Send the resized image
+        res.sendFile(resizedImagePath);
+        return;
+    } catch (error) {
+        console.error('Error processing image:', error);
+        res.status(500).json({
+            error: 'Failed to process image',
+        });
+        return;
+    }
 });
 
 export default imageRoute;
