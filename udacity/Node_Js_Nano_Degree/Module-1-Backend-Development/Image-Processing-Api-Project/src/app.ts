18ce8bcd8ca08a70ddaacdc955189cf1fe4cf7aa
Omar Anwer <omar.anwer@thirdwayv.com>
2024-11-18 23:06:28 +0200

    remove default api route

--- udacity/Node_Js_Nano_Degree/Module-1-Backend-Development/Image-Processing-Api-Project/src/app.ts
+++ udacity/Node_Js_Nano_Degree/Module-1-Backend-Development/Image-Processing-Api-Project/src/app.ts
@@ -9,15 +9,10 @@ app.use('/api/v1', routes);
 
 app.use(express.static('./public'));
 
-app.get('/', (req: Request, res: Response): void => {
-    // res.sendFile(path.resolve(__dirname, './index.html'));
-    res.send('main api route');
-});
-
 app.all('*', (req: Request, res: Response): void => {
     res.status(404).send('<h1>Page Not Found!</h1>');
 });
 
 app.listen(port, () => {
-    console.log(`server started at localhost:${port}`);
+    console.log(`Server listening at http://localhost:${port}`);
 });
